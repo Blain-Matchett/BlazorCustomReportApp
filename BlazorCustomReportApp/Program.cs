c5efd3484e92d550534938a02d5f3a534c940e5a
Paul Matchett <paul@ethor.com>
2023-04-04 13:00:21 -0400

    Added required references for Telerik Grid rendering

--- BlazorCustomReportApp/Program.cs
+++ BlazorCustomReportApp/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<DevContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 
+builder.Services.AddTelerikBlazor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
